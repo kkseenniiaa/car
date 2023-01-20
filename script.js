3f244de65b788d437f6061a3ad43c2d070b932f0
kkseenniiaa <98638900+kkseenniiaa@users.noreply.github.com>
2023-01-20 18:07:16 +0500

    js

---
+++ script.js
@@ -0,0 +1,48 @@
+let cells = document.querySelectorAll('#field td');
+console.log(cells);
+
+let i = 0;
+function start(cells) {
+    for (let cell of cells) {
+        cell.addEventListener('click', function step() {
+            if (i % 2 == 0) {
+                this.textContent = 'х';
+            } else {
+                this.textContent = 'о';
+            }
+
+            this.removeEventListener('click', step);
+            if (isWinner(cells) == true) {
+                alert(`Победитель ${this.textContent}`);
+            } else if (i == 8) {
+                alert('ничья');
+            };
+            i++;
+        })
+    }
+}
+
+start(cells);
+
+function isWinner(cells) {
+    let combs = [
+        [0, 1, 2], // comb
+        [3, 4, 5], // comb
+        [6, 7, 8], // comb
+        [0, 3, 6], // comb
+        [1, 4, 7], // comb
+        [2, 5, 8], // comb
+        [0, 4, 8], // comb
+        [2, 4, 6]  // comb
+
+    ]
+
+    for (let comb of combs) {
+        if (cells[comb[0]].textContent == cells[comb[1]].textContent && cells[comb[1]].textContent ==
+            cells[comb[2]].textContent && cells[comb[0]].textContent != '') {
+            return true;
+        }
+    }
+    return false;
+}
+
